f31da9913cd1da629419816afac7ac263e34880a
Thundelly <thundelly0609@gmail.com>
2021-04-24 16:31:27 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Input, Button, Alert } from 'rsuite';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [ num, setNum ] = useState('');
+  const [ prime, setPrime ] = useState(false);
+  const [ showResult, setShowResult ] = useState(false);
+
+  const handleInput = (input) => {
+    setNum(input);
+  }
+
+  const handleReset = () => {
+    setNum('');
+    setShowResult(false);
+  }
+
+  const verifyPrime = () => {
+
+    if (!isNaN(num) && num !== '') {
+      if (isPrime(num)) {
+        setPrime(true);
+      }
+      else {
+        setPrime(false);
+      }
+
+      setShowResult(true);
+    }
+
+    else if (num === '') {
+      Alert.warning('Please enter a number to proceed.')
+      setShowResult(false);
+      setPrime(false);
+    }
+
+    else {
+      Alert.error('Please enter a numeric value.');
+      setShowResult(false);
+      setPrime(false);
+    }
+  }
+
+  const isPrime = (num) => {
+    if (num <= 1)
+      return false;
+
+    for (let i = 2; i < num; i++)
+      if (num % i === 0)
+        return false;
+
+    return true;
+  }
+
+  const displayResult = () => {
+    if (showResult) {
+
+      const result_val = num;
+
+      return (
+        <p id='result'>
+          { prime ? num + ' is a prime number.' : num + ' is NOT a prime number.' }
+        </p>
+      )
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1> Prime Number Verifier </h1>
+
       </header>
-    </div>
+
+      <div className="App App-body">
+
+        <p>
+          Please enter a number below.
+        </p>
+
+        <Input
+          value={ num }
+          className="App-input"
+          onChange={ input => {
+            handleInput(input);
+            setShowResult(false);
+          } }
+        />
+
+        <Button
+          className="submit-button"
+          onClick={ verifyPrime }
+        >
+          Verify
+        </Button>
+
+        { displayResult() }
+
+        <Button
+          className='reset-button'
+          appearance='subtle'
+          onClick={ handleReset }>
+
+          Reset
+
+        </Button>
+
+      </div>
+
+    </div >
   );
 }
 
